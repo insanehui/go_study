c07cf925d61d19b1c8f41d871eb5a0dc60ff5f3c
insanehui <269200808@qq.com>
2016-11-16 21:18:56 +0800

    小修改一下，封装成函数

--- type_assertion/test.go
+++ type_assertion/test.go
@@ -6,9 +6,31 @@ import (
 	// "github.com/ghodss/yaml" // 找到另一个yaml库，但貌似没有强大的功能
 )
 
+func yaml_add(ydata string) string {
+
+	var m map[string]interface{}
+	yaml.Unmarshal([]byte(ydata), &m)
+
+	spec := m["spec"]
+
+	// 用类型断言，比reflect要简便一些
+	if spec, ok := spec.(map[interface{}]interface{}); ok {
+		sel := spec["selector"]
+		if sel, ok := sel.(map[interface{}]interface{}); ok {
+			mt := sel["matchLabels"]
+			if mt, ok := mt.(map[interface{}]interface{}); ok {
+				mt["xxx"] = 1111111
+			}
+		}
+	}
+
+	b, _ := yaml.Marshal(m)
+	return string(b)
+}
+
 func main() {
 
-	yfile := `
+	ydata := `
 kind: PersistentVolumeClaim
 apiVersion: v1
 metadata:
@@ -24,23 +46,6 @@ spec:
     matchLabels:
       name: "db"
 `
-	var m map[string]interface{}
-
-	yaml.Unmarshal([]byte(yfile), &m)
-
-	spec := m["spec"]
-
-	// 用类型断言，比reflect要简便一些
-	if spec, ok := spec.(map[interface{}]interface{}); ok {
-		sel := spec["selector"]
-		if sel, ok := sel.(map[interface{}]interface{}); ok {
-			mt := sel["matchLabels"]
-			if mt, ok := mt.(map[interface{}]interface{}); ok {
-				mt["xxx"] = 1234214
-			}
-		}
-	}
-
-	b, _ := yaml.Marshal(m)
+	b := yaml_add(ydata)
 	fmt.Printf("%s", b)
 }
