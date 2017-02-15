f71b2fcf560ab3b5814ef37e7b3f61f4bf540df2
insanehui <269200808@qq.com>
2017-02-15 10:45:03 +0800

    暂时封装了一个函数，但get的参数还无法传递

--- test/http_proxy.go
+++ test/http_proxy.go
@@ -69,6 +69,64 @@ func singleJoiningSlash(a, b string) string {
 	return a + b
 }
 
+func RProxy(url_path string) http.HandlerFunc {
+
+	target, _ := url.Parse(url_path)
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		o := new(http.Request)
+
+		*o = *r
+
+		o.Host = target.Host
+		o.URL.Scheme = target.Scheme
+		o.URL.Host = target.Host
+		o.URL.Path = singleJoiningSlash(target.Path, o.URL.Path)
+
+		if q := o.URL.RawQuery; q != "" {
+			o.URL.RawPath = o.URL.Path + "?" + q
+		} else {
+			o.URL.RawPath = o.URL.Path
+		}
+
+		o.URL.RawQuery = target.RawQuery
+
+		o.Proto = "HTTP/1.1"
+		o.ProtoMajor = 1
+		o.ProtoMinor = 1
+		o.Close = false
+
+		transport := http.DefaultTransport
+
+		res, err := transport.RoundTrip(o)
+
+		if err != nil {
+			log.Printf("http: proxy error: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		hdr := w.Header()
+
+		for k, vv := range res.Header {
+			for _, v := range vv {
+				hdr.Add(k, v)
+			}
+		}
+
+		for _, c := range res.Cookies() {
+			w.Header().Add("Set-Cookie", c.Raw)
+		}
+
+		w.WriteHeader(res.StatusCode)
+
+		if res.Body != nil {
+			io.Copy(w, res.Body)
+		}
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	o := new(http.Request)
 
@@ -122,7 +180,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", RProxy("http://localhost:8084/expand_blueprint"))
 	log.Println("Start serving on port 1234")
 	http.ListenAndServe(":1234", nil)
 }
